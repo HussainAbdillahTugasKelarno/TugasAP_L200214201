ecdbdb97595c5bca0bcaef6bfa1409b17817bf9e
Hussain Abdillah Tugas Kelarno <l200214201@student.ums.ac.id>
2021-12-22 21:50:36 +0700

    Mengubah tulisan tanggal dan menambahkan fungsi menghitung konsonan

--- pajak/myutils.py
+++ pajak/myutils.py
@@ -1,3 +1,5 @@
+from time import ctime
+
 def number_of_vowels(s):
     vowels = 'aiueo'
     c = 0
@@ -5,6 +7,14 @@ def number_of_vowels(s):
         if i in vowels:
             c = c + 1
     return c
+
+def number_of_consonants(x):
+    consonants = 'qwrtypsdfghjklzxcvbnm'
+    p = 0
+    for i in x.lower():
+        if i in consonants:
+            p = p + 1
+    return p
 
 day = { 'Sun' : 'Ahad',
         'Mon' : 'Senin',
@@ -26,3 +36,13 @@ month = {'Jan' : 'Januari',
          'Oct' : 'Oktober',
          'Nov' : 'November',
          'Dec' : 'Desember'}
+
+def ubahwaktu():
+    now = ctime()
+    _day    = now[0:3]
+    _month  = now[4:7]
+    _date   = now[8:10]
+    _clock  = now[11:19]
+    _year   = now[20:24]
+    sekarang = day[_day]+' '+ _date +' '+ month[_month]+' '+ _year+' '+ "Pukul "+' '+ _clock
+    return sekarang
--- pajak/views.py
+++ pajak/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from time import ctime
 from .myutils import number_of_vowels
+from .myutils import number_of_consonants
+from .myutils import ubahwaktu
 
 def coba(request):
-    now = ctime()
+    s = ubahwaktu()
     context = {
         'judul' : 'Tugas AP 2021',
         'nama'  : 'Hussain Abdillah Tugas Kelarno',
         'NIM'   : 'L200214201',
-        'waktu' : now,
+        'waktu' : s,
         'pesan' : 'Hello my friends, apa kabar ? I hope everything is good.',
     }
 
     if request.POST:
         st = request.POST.get('kalimat')
         v = number_of_vowels(st)
-        context.update( { 'kal'    : st, 
-                          'vowels' : v  } )
+        c = number_of_consonants(st)
+        context.update( { 'kal'        : st, 
+                          'vowels'     : v,  
+                          'consonants' : c  } )
 
     return render(request, 'pajak/index.html', context)
